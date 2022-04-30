58fce114a66f06db0b539927ab8f0b5af7bdcb77
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-04-30 15:32:13 +0300

    added twitter and og meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,12 +26,30 @@ const Home: NextPage = () => {
         <title>Subhub | track your subscriptions</title>
         <meta
           name="description"
-          content="Ahoy! Share and manage your subscriptions with your friends.  Help us democratize software "
+          content="Ahoy! Share and manage your subscriptions with your friends.  No more surpise billings "
         />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐳</text></svg>"
         />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:title"
+          content="Subhub | track your subscriptions"
+        />
+        <meta
+          name="twitter:description"
+          content="Ahoy! Share and manage your subscriptions with your friends. No more surpise billings."
+        />
+        <meta name="twitter:image" content="/apex.png" />
+
+        <meta property="og:title" content="Subhub | track your subscriptions" />
+        <meta
+          property="og:description"
+          content="Ahoy! Share and manage your subscriptions with your friends. No more surpise billings."
+        />
+        <meta property="og:image" content="/apex.png" />
+        <meta property="og:url" content="https://subhub.app" />
       </Head>
       {!loggedInUser ? <NormalView /> : <LoggedInView />}
     </div>
