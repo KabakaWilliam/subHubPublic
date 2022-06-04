74d2fbfb136f52c3aaeb0a3c0eb6d987745d1757
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-06-04 18:16:01 +0300

    changed button

--- components/AddSubscriptionButton.tsx
+++ components/AddSubscriptionButton.tsx
@@ -2,10 +2,10 @@ import { PlusIcon } from "@heroicons/react/outline";
 
 const AddSubscriptionButton = () => {
   return (
-    <div className="fixed bottom-[-3px] right-[-20px] md:right-[30px] w-[200px] h-[200px] flex justify-center items-center z-50  ">
+    <div className="fixed bottom-[-40px] md:bottom-[-3px] right-[-20px] md:right-[30px] w-[200px] h-[200px] flex justify-center items-center z-50  ">
       <button
         //   onClick={() => addProject()}
-        className="w-[100px] h-[100px] bg-[#7C0C7C] rounded-[100%] shadow-lg shadow-[#5e6665] hover:bg-[#d6d5c7] "
+        className="w-[50px] h-[50px] md:w-[100px] md:h-[100px] bg-[#7C0C7C] rounded-[100%] shadow-lg shadow-[#2b2d2d] hover:bg-[#d6d5c7] "
       >
         <PlusIcon className="stroke-1 text-white" />
       </button>
